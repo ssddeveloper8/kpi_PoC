cfad738f17c686c3fa99b2814c69b87acd4dd175
ssddeveloper8 <ssddeveloper8@gmail.com>
2026-04-21 17:57:57 +0530

    Final cleanup: removed old modules and cache files

--- kpi/kpi_parser.py
+++
@@ -1,8 +0,0 @@
-def extract_kpi_name_fallback(query: str):
-    words = query.split()
-
-    for i, w in enumerate(words):
-        if w.lower() == "kpi" and i + 1 < len(words):
-            return words[i + 1]
-
-    return None
--- tag/tag_parser.py
+++
@@ -1,8 +0,0 @@
-def extract_tag_name_fallback(query: str):
-    words = query.split()
-
-    for i, w in enumerate(words):
-        if w.lower() == "tag" and i + 1 < len(words):
-            return words[i + 1]
-
-    return None
